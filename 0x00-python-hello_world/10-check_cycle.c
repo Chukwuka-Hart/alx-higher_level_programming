4ce409200e1922cfd5a95432da47aebb7d4417db
Chukwuka-Hart <chukwukahart@gmail.com>
2023-03-06 14:58:29 -0800

    Hello world

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * check_cycle - checks if a singly linked list has a cycle
+ * @list: the singly linked list to examine
+ *
+ * Return: 1 if cycle is present or 0 if not
+ */
+
+int check_cycle(listint_t *list)
+{
+
---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Holberton School"
+first_character = str[:9]
+print(str * 3)
+print(first_character)
